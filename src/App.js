53c1fc7ff8c5dc396b34e219aa61301a8dd05d40
MarcusMo <marcus400.1@gmail.com>
2022-09-07 23:07:14 +0800

    added login, register pages. to continue on validations and jwt

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import { Routes, Route } from "react-router-dom";
 import React from "react";
 import "./App.css";
 import TrendingMovies from "./components/movie/trending/TrendingMovies";
+import Register from "./components/register/Register";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/movies" element={<TrendingMovies />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
+      <ToastContainer />
     </div>
   );
 }
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,72 @@
+import {useState} from 'react'
+import {toast} from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
+
+
+function Login() {
+    const navigate = useNavigate()
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+    })
+
+    const handleChange = e => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        fetch(`http://localhost:8000/api/v1/users/login`, {
+            method: 'POST',
+            body: JSON.stringify(formData),
+            headers: {
+                'Content-type': 'application/json',
+            },
+        })
+        .then(response => {
+            return response.json()
+        })
+        .then(jsonResponse => {
+            if (jsonResponse.error) {
+                toast.error(jsonResponse.error)
+                return
+            }
+
+            toast.success("Login Successful!")
+
+            // store the token into localstorage / cookie
+            localStorage.setItem('user_token', jsonResponse.token)
+
+            navigate('/')
+        })
+        .catch(err => {
+            toast.error(err.message)
+        })
+    }
+
+    return (
+        <div className="login-page">
+            <h1 className="my-5">Login</h1>
+
+            <div className="container">
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="username" className="form-label">Username</label>
+                        <input type="username" className="form-control" id="username" name="username" value={formData.username} onChange={handleChange} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="password" name="password" value={formData.password} onChange={handleChange} />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Login</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login
