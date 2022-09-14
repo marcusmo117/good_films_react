1a2a88926b7e6c8309e844067e2f59411936de05
MarcusMo <marcus400.1@gmail.com>
2022-09-15 00:16:38 +0800

    link and dropdown still broken

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Navbar, Nav, NavDropdown, Container, Button, Form } from 'react-bootstrap';
 import LogoutComp from '../logout/Logout';
+import {Link, NavLink} from "react-router-dom"
 
 function Navibar({tokenState, user, setTokenState}) {
   
@@ -7,8 +8,12 @@ function Navibar({tokenState, user, setTokenState}) {
     <Navbar bg="dark" variant="dark" sticky="top" expand="lg" style={{color:"white"}}>
       {tokenState ?
         <Container fluid className="justify-content-start">
-          <Navbar.Brand href="/">Home (feed)</Navbar.Brand>
-          <Nav.Link href="/">Browse movies</Nav.Link>
+          <Navbar.Brand>
+            <Link to={"/"} style={{ color: 'inherit', textDecoration: 'inherit' }}>Home (feed)</Link>
+          </Navbar.Brand>
+          <Nav.Link>
+            <Link to={"/"} style={{ color: 'inherit', textDecoration: 'inherit' }}>Browse movies</Link>
+          </Nav.Link>
           <Form className="d-flex mx-auto">
             <Form.Control
               type="search"
@@ -18,23 +23,31 @@ function Navibar({tokenState, user, setTokenState}) {
             />
             <Button variant="success">Search</Button>
           </Form>
-          <NavDropdown className="mx-4" title={"Hi, " + user + "!"} id="navbarScrollingDropdown" align="end">
-          <NavDropdown.Item href={'/profiles/' + user}>Profile</NavDropdown.Item>
-          <NavDropdown.Item href="#action4">
-            Following
-          </NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item>
-            <LogoutComp setTokenState={setTokenState}/>
-          </NavDropdown.Item>
+          <NavDropdown className="mx-4" title={"Hi, " + user + "!"} id="navbarDropdown" align="end">
+            <NavDropdown.Item id="profileDropdown">
+              <NavLink to={"/profiles/" + user} style={{ color: 'inherit', textDecoration: 'inherit' }}>Profile</NavLink>
+            </NavDropdown.Item>
+            {/* <NavDropdown.Item id="followingDropdown">
+              <Link to={"/profiles/" + user} style={{ color: 'inherit', textDecoration: 'inherit' }}>Following</Link>
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item id="logoutDropdown">
+              <LogoutComp setTokenState={setTokenState}/>
+            </NavDropdown.Item> */}
           </NavDropdown>
         </Container>
       :
         <Container fluid className="justify-content-start">
-          <Navbar.Brand href="/">Browse movies</Navbar.Brand>
+          <Navbar.Brand>
+            <Link to={"/"} style={{ color: 'inherit', textDecoration: 'inherit' }}>Browse movies</Link>
+          </Navbar.Brand>
           <Container fluid className="d-flex justify-content-end">
-            <Nav.Link className="d-inline-block mx-4" href="/login">Login</Nav.Link>
-            <Nav.Link className="d-inline-block mx-4" href="/register">Register</Nav.Link>
+            <Nav.Link className="d-inline-block mx-4">
+              <Link to={"/login"} style={{ color: 'inherit', textDecoration: 'inherit' }}>Login</Link>
+            </Nav.Link>
+            <Nav.Link className="d-inline-block mx-4">
+              <Link to={"/register"} style={{ color: 'inherit', textDecoration: 'inherit' }}>Register</Link>
+            </Nav.Link>
           </Container>
         </Container>
       }
