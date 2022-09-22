aef392ffd7a18183b6451d1f0ffeb7c256a3ee5a
lekpeng <lekpeng96@hotmail.com>
2022-09-23 02:01:07 +0800

    fix: merge conflict in Auth

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -3,39 +3,6 @@ import jwt_decode from "jwt-decode";
 import { DateTime } from "luxon";
 
 function Auth(props) {
-<<<<<<< HEAD
-    // write our auth checking logic here in a single place
-
-    // retrieve token from local storage 
-    const token = localStorage.getItem('user_token')
-    
-    // if it does not exist, redirect to login page 
-    if (!token) {
-        return (
-            <Navigate to={'/login'} />
-        )
-    }
-
-    // if it exists, check if token expired
-    // if expired, purge local storage, redir to login page 
-    // if token exp ok, render props.component 
-    const user = jwt_decode(token)
-
-    const now = DateTime.now().toUnixInteger()
-
-    if (user.exp < now) {
-        localStorage.removeItem('user_token')
-        return (
-            <Navigate to={'/login'} />
-        )
-    }
-
-    return (
-        // props.component refers to components that is put in on app.jsx
-        <props.component setFollowState={props.setFollowState} followState={props.followState}></props.component>
-    )
-=======
-  console.log("auth props", props);
   // write our auth checking logic here in a single place
 
   // retrieve token from local storage
@@ -61,10 +28,9 @@ function Auth(props) {
   return (
     // props.component refers to components that is put in on app.jsx
     <props.component
-      followingCount={props.followingCount}
-      setFollowingCount={props.setFollowingCount}></props.component>
+      setFollowState={props.setFollowState}
+      followState={props.followState}></props.component>
   );
->>>>>>> fix: solving warnings and errors
 }
 
 export default Auth;
